6a6d2a3da80fd00f25eb089ecfc8209926a5612b
Maame A. Apenteng <maame.a.apenteng@avanade.com>
2017-11-24 14:42:16 -0500

    set-up displaying a list of products

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -4,11 +4,13 @@ import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 
 import { AlbumInfo } from './albuminfo';
+import { Product } from './product';
 
 @Injectable()
 export class ProductService {
 
   private _albumUrl = '../assets/album.json';
+  private _productsUrl = '../assets/products.json';
 
   constructor(private _http: Http) { }
 
@@ -16,4 +18,7 @@ export class ProductService {
     return this._http.get(this._albumUrl).map((response) => <AlbumInfo>response.json());
   }
 
+  getProducts(): Observable<Product[]> {
+    return this._http.get(this._productsUrl).map((response) => <Product[]>response.json());
+  }
 }
